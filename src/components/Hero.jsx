e1f0c0585791032e1f5bd2e8142309940d35935f
fasil <fasilsebsibe27@gmail.com>
2025-09-05 18:01:06 +0300

    adding hero component and edit in navbar

---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <div className='flex flex-col items-start justify-center px-6 md:px-16 lg:px-24 xl:px-32 text-white bg-[url("/src/assets/heroImage.png")] bg-no-repeat bg-cover bg-center h-screen'>
+      <p>The Ultimate Hotel Experience</p>
+      <h1>Discover Your Perfect Geteway Destination</h1>
+      <p>Unparalleded Luxury and comfort await at the world's most exclusive hotels and resorts. start your journey today.</p>
+    </div>
+  )
+}
